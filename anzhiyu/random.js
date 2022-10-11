20a9693f8f1f843144886f4b4d9e00709423dc7c
VaIOReTto1 <1302140648@qq.com>
2022-10-11 16:21:06 +0800

    Site updated: 2022-10-11 16:21:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/4a17b156.html","posts/d87f7e0c.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/d87f7e0c.html","posts/4a17b156.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
