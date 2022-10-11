52427e018924937b36395e74ecc2444ea32466b0
VaIOReTto1 <1302140648@qq.com>
2022-10-11 23:15:48 +0800

    Site updated: 2022-10-11 23:15:48

--- live2d/js/index.js
+++ live2d/js/index.js
@@ -9,12 +9,12 @@ function browserRedirect() {
     var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
     var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
     if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {} else {
-        document.write('<link rel="stylesheet" href="/live2d/css/live2d.css" /><div id="landlord"> <div class="message" style="opacity:0.5"></div> <canvas id="live2d" width="280" height="250" class="live2d"></canvas> <div class="hide-button"><font color="#FFFFFF">隐藏</font></div></div>');
-        document.write('<script type="text/javascript"> var message_Path = "/live2d/"</script>');
+        document.write('<link rel="stylesheet" href="/live2d/css/live2d.css" /><div id="landlord"> <div class="message" style="opacity:0.5"></div> <canvas id="live2d" width="350" height="600" class="live2d"></canvas> <div class="hide-button"><font color="#FFFFFF">隐藏</font></div></div>');
+        document.write('<script type="text/javascript"> var message_Path = "/live2d/"</script>')
         document.write('<script type="text/javascript"> var home_Path = "https://vaioretto1.github.io/" </script>');
         document.write('<script type="text/javascript" src="/live2d/js/live2d.js"></script>');
         document.write('<script type="text/javascript" src="/live2d/js/message.js"></script>');
-        document.write('<script type="text/javascript"> loadlive2d("live2d", "/live2d/model/shifuku/shifuku.model.json");</script>');
+        document.write('<script type="text/javascript"> loadlive2d("live2d", "/live2d/model/shifuku/14.json");</script>');
     }
 }
 browserRedirect();
